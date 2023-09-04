f1dd103b403dd9049ae721c6b9c25e7da758bcc0
lfbook0831 <116310068+lfbook0831@users.noreply.github.com>
2023-09-04 18:47:38 +0000

    fixed errors
    i fixed errors with )

--- index.js
+++ index.js
@@ -1,53 +1,30 @@
-
-
-
 const http = require("http");
+const fs = require("fs").promises;
 
-
-
-const fs =require("fs").
-  
-  
-  const requestListener = function(req, res) {
-
+  const requestListener = function (req, res) {
   console.log(req.url);
 
   if(req.url === "/") {
-    fs.readFile( __dirname + "/page.html" )
-    .then(
-      contents => {
-
+    fs.readFile( __dirname + "/page.html").then((contents) => {
         res.setHeader ("Content-Type", "text/html; charset=UTF-8");
-
         res.writeHead(200);
+        res.end(contents);      
+      });
 
-        res.end(contents);
-        
-      }
-    )
   } else {
-
-    fs.readFile(__dirname + "/data.json")
-    .then(contents => {
-
+    fs.readFile(__dirname + "/data.json").then((contents) => {
       res.setHeader("Content-Type", "application/json; charset=UTF-8");
-
-       res.writeHead(200);
-
-        res.end(contents);
-    });
-  }
+      res.writeHead(200);
+      res.end(contents);
+     });
+    }
   };
 
-const server = http.createserver(requestListener);;
+const server = http.createServer(requestListener);
 
 const host = "0.0.0.0";
 const port = "8080";
 
-server.listen(
-  port,
-  host,
-  () => {
+server.listen(port, host, () => {
     console.log('Server is running');
-  }
-);
+});
